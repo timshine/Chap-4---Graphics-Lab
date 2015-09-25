124e7e154ac5a5f7178c391575828305f38b4122
unknown <4068926@HS222-08.vtsd.com>
2015-09-25 08:48:48 -0400

    Graphics Lab

---
+++ src/GraphicsLab.java
@@ -0,0 +1,43 @@
+/**************************************
+ *     Timothy Shine                  * 
+ *     AP Graphics Lab Chap 4         * 
+ *     9/25/2015                      * 
+ * 					                  *
+ **************************************/
+
+import java.awt.*;
+import java.applet.*;
+
+public class GraphicsLab extends Applet {
+	public void paint(Graphics g)
+	{
+		// DRAW CUBE
+		g.drawRect(100, 100, 300, 300);
+
+
+
+		// DRAW SPHERE
+
+
+
+
+		// DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
+
+
+
+
+		// DRAW APCS
+
+
+
+
+		// DRAW PACMEN FLOWER
+
+
+
+
+	}
+
+}
+
+
